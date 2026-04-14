48b9d425c4512a5c2cf2e2a03d7cbfc5aa5123ab
kimura <c0c250560b@edu.ed.ac.jp>
2026-04-14 15:40:54 +0900

    練習３と４：こうかとんの画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,16 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg") #練習１
+    bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    kk_img = pg.image.load("fig/3.png")#練習3.1
+    kk_img =pg.transform.flip(kk_img, True, False)#練習3.2
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])#練習２
+        screen.blit(bg_img, [0, 0])#練習2
+        screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
         clock.tick(10)
